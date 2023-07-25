9173858079d72fc6dc86f0ee34f33fd85ccf847c
Angela <angelakulola4@gmail.com>
2023-07-25 21:29:46 +0300

    Add routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,81 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  # Event Creation
+  post '/events' do
+    event_data = JSON.parse(request.body.read)
+    event = Event.create(event_data)
+    event.to_json
+  end
+  
+  # Event Listing
+  get '/events' do
+    events = Event.all
+    events.to_json
+  end
+  
+  # Event Details
+  get '/events/:id' do
+    event = Event.find(params[:id])
+    event.to_json
+  end
+  
+  # Event Deletion
+  delete '/events/:id' do
+    event = Event.find(params[:id])
+    event.destroy
+    event.to_json
+  end
+  
+  # Get events belonging to a user
+  get '/users/:user_id/events' do
+    user = User.find(params[:user_id])
+    events = user.events
+    events.to_json
+  end
+  
+  # Get the user associated with an event
+  get '/events/:event_id/user' do
+    event = Event.find(params[:event_id])
+    user = event.user
+    user.to_json
+  end
+  
+  # Get attendees for an event
+  get '/events/:event_id/attendees' do
+    event = Event.find(params[:event_id])
+    attendees = event.users
+    attendees.to_json
+  end
+  
+  # Get events for a user
+  get '/users/:user_id/events' do
+    user = User.find(params[:user_id])
+    events = user.events
+    events.to_json
+  end
+  
+  # Create an attendee for an event 
+  post '/events/:event_id/attendees' do
+    event = Event.find(params[:event_id])
+    attendee_data = JSON.parse(request.body.read)
+    attendee = event.attendees.create(attendee_data)
+    attendee.to_json
+  end
+  
+  # Update an attendee
+  put '/attendees/:id' do
+    attendee = Attendee.find(params[:id])
+    attendee_data = JSON.parse(request.body.read)
+    attendee.update(attendee_data)
+    attendee.to_json
+  end
+  
+  # Delete an attendee
+  delete '/attendees/:id' do
+    attendee = Attendee.find(params[:id])
+    attendee.destroy
+    attendee.to_json
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+puts  "Deleting all seeds"
+    Event.delete_all
+    User.delete_all
+    Attendee.delete_all
+
 puts "🌱 Seeding spices..."
 
 # Seed your database here
@@ -10,9 +15,8 @@ event1 = Event.create(
   description: 'An enchanting event for the showing of Barbie the movie',
   start_time: Time.now + 1.week,
   end_time: Time.now + 1.week + 3.hours,
-  location: 'Wonderland',
-  user: user1,
-  agendas: ['Red carpet entry', 'Movie screening', 'Themed after-party']
+  location: 'Los Angeles, California',
+  agenda: ['Red carpet entry', 'Movie screening', 'Themed after-party']
 )
 
 event2 = Event.create(
@@ -20,9 +24,8 @@ event2 = Event.create(
   description: 'A magical costume ball at the Fairy Palace',
   start_time: Time.now + 2.weeks,
   end_time: Time.now + 2.weeks + 5.hours,
-  location: 'Fairy Palace',
-  user: user2,
-  agendas: ['Red carpet entry', 'Dancing under the stars', 'Best costume competition']
+  location: '1010 Vienna, Bankgasse 8',
+  agenda: ['Red carpet entry', 'Dancing under the stars', 'Best costume competition']
 )
 
 event3 = Event.create(
@@ -30,13 +33,52 @@ event3 = Event.create(
   description: 'A  book club gathering for bookworms',
   start_time: Time.now + 3.weeks,
   end_time: Time.now + 3.weeks + 2.hours,
-  location: 'Cozy Library',
-  user: user1,
-  agendas: ['Book discussion', 'Snacks and refreshments', 'Book exchange']
+  location: '1557 Huaihai Zhong Lu, Xuhui District, Shanghai',
+  agenda: ['Book discussion', 'Snacks and refreshments', 'Book exchange']
 )
 
-invited_guest1 = InvitedGuest.create(event: event1, user: user2)
-invited_guest2 = InvitedGuest.create(event: event2, user: user1)
-invited_guest3 = InvitedGuest.create(event: event3, user: user2)
+event4 = Event.create(
+    title: 'Summer Music Festival',
+    description: 'Join us for a fun-filled music festival in the park!',
+    start_time: DateTime.new(2023, 8, 15, 12, 0),
+    end_time: DateTime.new(2023, 8, 15, 22, 0),
+    location: 'Central Park, New York',
+    agenda: '12:00 PM - Opening Ceremony\n02:00 PM - Live Performances\n08:00 PM - Closing Party'
+)
+
+event5 = Event.create(
+    title: 'Tech Conference',
+    description: 'Discover the latest trends in technology and innovation.',
+    start_time: DateTime.new(2023, 9, 20, 9, 0),
+    end_time: DateTime.new(2023, 9, 22, 18, 0),
+    location: 'Convention Center, San Francisco',
+    agenda: '09:00 AM - Registration and Welcome\n11:00 AM - Keynote Speech\n03:00 PM - Breakout Sessions\n06:00 PM - Networking'
+  )
+
+event6 = Event.create(
+    title: 'Art Exhibition',
+    description: 'Explore a stunning collection of contemporary art.',
+    start_time: DateTime.new(2023, 10, 5, 10, 0),
+    end_time: DateTime.new(2023, 10, 7, 20, 0),
+    location: 'Art Gallery, London',
+    agenda: '10:00 AM - Gallery Opening\n02:00 PM - Artist Talks\n07:00 PM - Cocktail Reception'
+  )
+
+event7 = Event.create(
+    title: 'Food Festival',
+    description: 'Taste the best cuisines from around the world.',
+    start_time: DateTime.new(2023, 11, 15, 12, 0),
+    end_time: DateTime.new(2023, 11, 17, 22, 0),
+    location: 'Fairgrounds, Chicago',
+    agenda: '12:00 PM - Food Tasting\n03:00 PM - Cooking Demonstrations\n08:00 PM - Food Awards'
+  )
+
+attendee1 = Attendee.create(event: event1, user: user2)
+attendee2 = Attendee.create(event: event2, user: user1)
+attendee3 = Attendee.create(event: event3, user: user2)
+attendee4 = Attendee.create(event: event1, user: user1)
+attendee5 = Attendee.create(event: event1, user: user2)
+attendee6 = Attendee.create(event: event2, user: user1)
+attendee7 = Attendee.create(event: event2, user: user2)
 
 puts "✅ Done seeding!"
